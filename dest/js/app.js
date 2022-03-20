2f0a5592e0cd8fd07140c20783f3ed262236f555
Andrii <Luckyhidalgo@gmail.com>
2022-03-20 14:50:41 +0200

    Content and Thank you page :::  - created;

--- dest/js/app.js
+++ dest/js/app.js
@@ -104,7 +104,8 @@ window.addEventListener('load', function (ev) {
   // COMMON
   _common_common__WEBPACK_IMPORTED_MODULE_0__["default"].initLoad(); // MACROS
 
-  /* FORM 1 INPUT */
+  /* FORM 1 INPUT
+  * ============================== */
 
   document.querySelector('[contentForm1-input-js]').addEventListener('input', function (ev) {
     var el = ev.currentTarget;
@@ -117,6 +118,57 @@ window.addEventListener('load', function (ev) {
       document.querySelector('[contentForm1-btn-js]').classList.remove('is-active');
     }
   }, false);
+  document.querySelector('[contentForm1-btn-js]').addEventListener('click', function (ev) {
+    document.querySelector('.content__wrapper-1').style.display = 'none';
+    document.querySelectorAll('.content__wrapper-2').forEach(function (val, idx) {
+      val.style.display = 'block';
+    });
+  }, false);
+  /* FORM 2 INPUT
+  * ============================== */
+
+  document.querySelector('[contentForm2-input1-js]').addEventListener('input', function (ev) {
+    var el = ev.currentTarget;
+
+    if (el.value.length > 2) {
+      el.closest('label').classList.add('is-open');
+    } else {
+      el.closest('label').classList.remove('is-open');
+    }
+  }, false);
+  /* DROPDOWN LINK TOGGLE
+  * ============================== */
+
+  document.querySelectorAll('.content__form-dropdown a').forEach(function (val, idx) {
+    val.addEventListener('click', function (ev) {
+      var el = ev.currentTarget;
+      el.closest('label').classList.remove('is-open');
+      document.querySelector('[contentForm2-input1-js]').value = el.innerText;
+      document.querySelector('[contentForm2-input2-js]').style.display = 'flex';
+    }, false);
+  });
+  /* FORM 3 INPUT
+  * ============================== */
+
+  document.querySelector('[contentForm2-input2-js] input').addEventListener('input', function (ev) {
+    var el = ev.currentTarget;
+
+    if (el.value.length > 2) {
+      document.querySelector('.content__box-container').classList.add('is-show');
+    } else {
+      document.querySelector('.content__box-container').classList.remove('is-show');
+    }
+  }, false);
+  /* BOX TOGGLE
+  * ============================== */
+
+  document.querySelectorAll('.content__box').forEach(function (val, idx) {
+    val.addEventListener('click', function (ev) {
+      var el = ev.currentTarget;
+      el.classList.toggle('is-active');
+      document.querySelector('.content__box-header span').innerText = document.querySelectorAll('.content__box.is-active').length;
+    }, false);
+  });
 }, false); // EVENT LISTENER - SCROLL
 // ========================================
 
--- src/js/app.js
+++ src/js/app.js
@@ -9,7 +9,8 @@ window.addEventListener('load', (ev) => {
   Common.initLoad();
 
   // MACROS
-  /* FORM 1 INPUT */
+  /* FORM 1 INPUT
+  * ============================== */
   document.querySelector('[contentForm1-input-js]').addEventListener('input', (ev) => {
 
     const el = ev.currentTarget;
@@ -23,6 +24,68 @@ window.addEventListener('load', (ev) => {
     }
 
   }, false);
+  document.querySelector('[contentForm1-btn-js]').addEventListener('click', (ev) => {
+    document.querySelector('.content__wrapper-1').style.display = 'none';
+    document.querySelectorAll('.content__wrapper-2').forEach((val, idx) => {
+      val.style.display = 'block';
+    })
+  }, false);
+
+  /* FORM 2 INPUT
+  * ============================== */
+  document.querySelector('[contentForm2-input1-js]').addEventListener('input', (ev) => {
+
+    const el = ev.currentTarget;
+
+    if(el.value.length > 2) {
+      el.closest('label').classList.add('is-open');
+    } else {
+      el.closest('label').classList.remove('is-open');
+    }
+
+  }, false);
+
+  /* DROPDOWN LINK TOGGLE
+  * ============================== */
+  document.querySelectorAll('.content__form-dropdown a').forEach((val, idx) => {
+    val.addEventListener('click', (ev) => {
+
+      const el = ev.currentTarget;
+
+      el.closest('label').classList.remove('is-open');
+      document.querySelector('[contentForm2-input1-js]').value = el.innerText;
+      document.querySelector('[contentForm2-input2-js]').style.display = 'flex';
+
+    }, false);
+  });
+
+  /* FORM 3 INPUT
+  * ============================== */
+  document.querySelector('[contentForm2-input2-js] input').addEventListener('input', (ev) => {
+
+    const el = ev.currentTarget;
+
+    if(el.value.length > 2) {
+      document.querySelector('.content__box-container').classList.add('is-show');
+    } else {
+      document.querySelector('.content__box-container').classList.remove('is-show');
+    }
+
+  }, false);
+
+  /* BOX TOGGLE
+  * ============================== */
+  document.querySelectorAll('.content__box').forEach((val, idx) => {
+    val.addEventListener('click', (ev) => {
+
+      const el = ev.currentTarget;
+
+      el.classList.toggle('is-active');
+
+      document.querySelector('.content__box-header span').innerText = document.querySelectorAll('.content__box.is-active').length;
+
+    }, false);
+  });
 
 }, false);
 
